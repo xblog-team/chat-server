980a34beb763d9d26e3f58c77d9a7de6ed38b2af
강경훈 <kkh5535@naver.com>
2024-08-12 14:34:07 +0900

    Feat: 환경 구성

---
+++ src/test/java/com/xblog/chatApi/ChatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xblog.chatApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
