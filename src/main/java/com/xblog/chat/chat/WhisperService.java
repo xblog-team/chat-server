db2af35c33892d812232bb9f5898bc47a6998f4b
강경훈 <kkh5535@naver.com>
2024-08-19 16:26:05 +0900

    Feat: 귓속말 기능 구현

---
+++ src/main/java/com/xblog/chat/chat/WhisperService.java
@@ -0,0 +1,51 @@
+package com.xblog.chat.chat;
+
+import org.springframework.lang.Nullable;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageType;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import com.xblog.chat.message.WhisperMessage;
+import com.xblog.chat.user.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class WhisperService {
+	private final SimpMessagingTemplate messagingTemplate;
+	private final UserService userService;
+
+	public void whisper(SimpMessageHeaderAccessor headerAccessor, WhisperMessage whisperMessage) {
+		String nickname = userService.getNickname(headerAccessor);
+		String receiver = whisperMessage.getReceiver();
+
+		String senderSessionId = headerAccessor.getSessionId();
+		String receiverSessionId = userService.getUserMap().get(receiver);
+
+		String messageForSender;
+
+		if (StringUtils.hasText(receiverSessionId)) {
+			messageForSender = "To " + receiver + ": " + whisperMessage.getContent();
+			String message = nickname + ": " + whisperMessage.getContent();
+			messagingTemplate.convertAndSendToUser(receiverSessionId, "/queue/whisper", message, createHeaders(
+				receiverSessionId));
+		} else {
+			messageForSender = receiver + "가 존재하지 않습니다.";
+		}
+
+		messagingTemplate.convertAndSendToUser(senderSessionId, "/queue/whisper", messageForSender,
+			createHeaders(senderSessionId));
+	}
+
+	private MessageHeaders createHeaders(@Nullable String sessionId) {
+		SimpMessageHeaderAccessor headerAccessor = SimpMessageHeaderAccessor.create(SimpMessageType.MESSAGE);
+		if (sessionId != null)
+			headerAccessor.setSessionId(sessionId);
+		headerAccessor.setLeaveMutable(true);
+		return headerAccessor.getMessageHeaders();
+	}
+}
