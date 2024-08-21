e7aef88e2ef7394fdc18b5c74461e5615d1d5671
강경훈 <kkh5535@naver.com>
2024-08-21 15:05:13 +0900

    Feat: Aspect 사용을 위한 config

---
+++ src/main/java/com/xblog/chat/config/AspectConfig.java
@@ -0,0 +1,9 @@
+package com.xblog.chat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class AspectConfig {
+}
